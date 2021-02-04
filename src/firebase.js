436c6bfe1b991a1c437f4d4821657c713b8f80fb
Luca Properzi <abramoproperzi@gmail.com>
2021-02-04 16:28:40 -0700

    implemented navbar component with styling and scroll animation

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkMWS9XRw3lap-p72ZHNuJYyE2HoPNoRs",
+  authDomain: "netflix-clone-8e43a.firebaseapp.com",
+  projectId: "netflix-clone-8e43a",
+  storageBucket: "netflix-clone-8e43a.appspot.com",
+  messagingSenderId: "894406665842",
+  appId: "1:894406665842:web:fa8c3df3528b52340f44eb",
+  measurementId: "G-3DPPG7D0LG"
+};
